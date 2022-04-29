a5d630c5d8d1c6315857ddcba381210c5200a8f7
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-04-29 16:30:12 +0200

    retrieve footer erased by mistake

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p class="aster">* Service à la personne, Vars (16), traite les données recueillies afin de pouvoir vous recontacter.<br>
+    Pour en savoir plus sur la gestion de vos données personnelles et pour exercer vos droits, reportez-vous à <a href="rgpd.html">la notice ci-jointe</a>.</p>
+        
+    <p>Numéro SIRET : 888 206 745 00018</p>
+        
+    <p>Icônes du site fournies gratuitement par <a href="http://www.flaticon.com">Flaticon</a></p>
+    <p>2022 - Site web réalisé par Alexis Boucherie</p>
+</footer>
